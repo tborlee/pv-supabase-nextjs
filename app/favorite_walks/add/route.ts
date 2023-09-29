9a7db952096aaec2e60e6406e9156d1f8c732c28
Thomas Borlée <thomas@massiverocket.com>
2023-09-29 14:08:33 +0200

    Revert "Fix add to favorite not working"
    This reverts commit 4abb49accb2885b5c233ced42a3e1d7fd905bb18.

--- app/favorite_walks/add/route.ts
+++ app/favorite_walks/add/route.ts
@@ -8,9 +8,10 @@ export async function POST(request: Request) {
   const {walk_id} = await request.json()
   const supabase = createRouteHandlerClient({cookies})
 
-  const userResponse = await supabase.auth.getUser();
+  // @ts-ignore
+  const {data: {session: {user}}} = await supabase.auth.getSession();
 
-  const response = await supabase.from("favorite_walks").upsert({walk_id, user_id: userResponse.data.user?.id}).select()
+  const response = await supabase.from("favorite_walks").upsert({walk_id, user_id: user.id}).select()
 
   if (response.error) {
     return new Response(JSON.stringify({error: response.error}), {status: 500});
