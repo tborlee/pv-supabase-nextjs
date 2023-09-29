f2668f5df73a5cbc14b26e04423f90fd0f36f46d
Thomas Borlée <thomas@massiverocket.com>
2023-09-29 14:17:47 +0200

    Revert "Trying edge runtime again, but with a potential fix"
    This reverts commit 97490a4674d32124c4ad04216d5195af9fad7980.

--- app/favorite_walks/remove/route.ts
+++ app/favorite_walks/remove/route.ts
@@ -1,5 +1,5 @@
-import {cookies} from 'next/headers'
 import {createRouteHandlerClient} from '@supabase/auth-helpers-nextjs'
+import {cookies} from 'next/headers'
 
 export const dynamic = 'force-dynamic'
 
