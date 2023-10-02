f0a26791a69974d8cb88da46f2de32503ac00a80
Thomas Borlée <thomas@massiverocket.com>
2023-10-02 10:43:34 +0200

    Pre-render only the closest walk dates, and return a 404 error if the parameter is not a date

--- app/[date]/page.tsx
+++ app/[date]/page.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import WalksContainer from "@/components/WalksContainer";
 import supabase from "@/utils/supabase";
+import {notFound} from "next/navigation";
+
+function withDiffMonths(diff: number) {
+  const date = new Date();
+  date.setMonth(date.getMonth() + diff);
+  return date.toISOString().substring(0, 10);
+}
 
 export async function generateStaticParams() {
-  const {data: dates} = await supabase.from('distinct_walk_dates').select('date')
+  const {data: dates} = await supabase.from('distinct_walk_dates').select('date').gte('date', withDiffMonths(-6)).lte('date', withDiffMonths(6))
 
   return dates!.map(({date}) => ({
     date,
@@ -11,6 +18,13 @@ export async function generateStaticParams() {
 }
 
 export default function Page({params}: { params: { date: string } }) {
+
+  const parsed = Date.parse(params.date);
+
+  if (isNaN(parsed)) {
+    return notFound();
+  }
+
   return (
     <>
       <WalksContainer date={params.date}/>
