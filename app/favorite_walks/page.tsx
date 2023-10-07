9b826b7c4013eebf6d74aafc8bc0360189e9ca39
Thomas Borlée <thomas@borlee.be>
2023-10-07 18:07:55 +0200

    Forgot the favorite page

--- app/favorite_walks/page.tsx
+++ app/favorite_walks/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import FavoriteWalks from "@/components/FavoriteWalks";
+import Header from "@/components/Header";
 
 export default function Page() {
   return (
     <>
+      <Header/>
       <h1>Favorite Walks</h1>
       <FavoriteWalks/>
     </>
